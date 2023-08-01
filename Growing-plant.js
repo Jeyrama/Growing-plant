35660d9dfaf200b98a9c2c03811343c00e85c96f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-31 19:46:22 -0700

    Update Growing-plant.js

--- Growing-plant.js
+++ Growing-plant.js
@@ -29,3 +29,19 @@ After day 10 --> 910
 
 
 // Solution
+
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  if (downSpeed > upSpeed) {
+    return -1;
+  }
+  
+  let height = downSpeed;
+  let days = 0;
+  do {
+    days++;
+    height -= downSpeed;
+    height += upSpeed;
+  } while (height < desiredHeight)
+  
+  return days;
+}
